83462ad7295a1424d1ecd41a623a72e723e25e62
IlyaNov <noil.01@mail.ru>
2023-03-31 15:54:46 +0300

    JaCoCoFinal

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.statistic;
 
-        import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-        import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
 
@@ -12,6 +12,18 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11};
+        long expected = 11;
 
         long actual = service.findMax(incomesInBillions);
 
